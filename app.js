48054a044155db62412c1fcfd6a4c75e71bded72
yiladien@hotmail.com <yiladien@hotmail.com>
2022-09-07 18:23:46 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,89 +1,21 @@
-// console.log("Hello Node!");
-
-// console.log(document);
-
-// var message = "Hello Node!";
-
-// var sum = 5 + 3;
-
-// console.log(message);
-
-// console.log(sum);
-
-// var commandLineArgs = process.argv;
-
-// console.log(commandLineArgs);
-
-// console.log(process);
-
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// const printProfileData = (profileDataArr) => {
-// This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log("================");
-
-// Is the same as this...
-//   profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem);
-//   });
-// };
-
-// printProfileData(profileDataArgs);
-
-// const generatePage = () => "Name: Jane, Gihub: janehub";
-// console.log(generatePage());
-
-// const generatePage = (userName, githubName) =>
-//   `Name: ${userName}, Github: ${githubName}`;
-// console.log(generatePage("Jane", "janehub"));
-
+const inquirer = require("inquirer");
 // const fs = require("fs");
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const generatePage = require("./src/page-template");
 
-// const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-// const generatePage = (userName, githubName) =>
-//   `
-//   <!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>Portfolio Demo</title>
-//   </head>
-
-//   <body>
-//     <h1>${name}</h1>
-//     <h2><a href="https://github.com/${github}">Github</a></h2>
-//   </body>
-//   </html>
-//   `;
-
-// fs.writeFile("index.html", generatePage(name, github), (err) => {
+// fs.writeFile("./index.html", pageHTML, (err) => {
 //   if (err) throw err;
 
 //   console.log("Portfolio complete! Check out index.html to see the output!");
 // });
 
-const generatePage = require("./src/page-template.js");
-
-const fs = require("fs");
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
